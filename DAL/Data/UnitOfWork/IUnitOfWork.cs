c759bb3d09644282ebb827c50728476f1642b61a
R0294410 <jeroen-baeten@hotmail.com>
2023-05-03 19:22:03 +0200

    Added repo's for all classes

--- DAL/Data/UnitOfWork/IUnitOfWork.cs
+++ DAL/Data/UnitOfWork/IUnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using JTO_Models;
+using JTO_MODELS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,17 @@ namespace DAL.Data.UnitOfWork
 {
     public interface IUnitOfWork : IDisposable
     {
+        IRepository<AgeCategory> AgeCategoryRepo { get; }
+        IRepository<Country> CountryRepo { get; }
+        IRepository<Course> CourseRepo { get; }
+        IRepository<Destination> DestionationRepo { get; }
+        IRepository<GroupTour> GroupTourRepo { get; }
+        IRepository<MedicalSheet> MedicalSheetRepo { get; }
+        IRepository<Participant> ParticipantRepo { get; }
+        IRepository<Person> PersonRepo { get; }
+        IRepository<Role> RoleRepo { get; }
+        IRepository<Theme> ThemeRepo { get; }
+        IRepository<Training> TrainingRepo { get; }
         IRepository<User> UserRepo { get; }
 
         int Save();
