b24269093b95419b7580e01397831bd81ceef64a
Kevin <r0389573@student.thomasmore.be>
2023-05-04 22:15:55 +0200

    first start at making view for grouptours

---
+++ JTO_WPF/ViewModels/GroupTourViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JTO_WPF.ViewModels
+{
+    internal class GroupTourViewModel : BaseViewModel
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            switch (parameter.ToString())
+            {
+                
+            }
+        }
+    }
+}
