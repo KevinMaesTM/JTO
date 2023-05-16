1a66628e3cd0aafe20ef59f4d51de3de57708402
maartend7 <maartendaems7@gmail.com>
2023-05-16 21:12:55 +0200

    Added table name users and example data

--- JTO_Models/User.cs
+++ JTO_Models/User.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JTO_Models
 {
+    [Table("Users")]
     public partial class User
     {
         public string Password { get; set; }
