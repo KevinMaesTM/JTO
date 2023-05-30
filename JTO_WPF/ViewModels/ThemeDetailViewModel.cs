58c9e907e3ce464530275a4d0576c1a544a5332d
Kevin Maes <kevin.maes@bignited.be>
2023-05-30 11:16:23 +0200

    finished work for Theme screens

--- JTO_WPF/ViewModels/ThemeDetailViewModel.cs
+++ JTO_WPF/ViewModels/ThemeDetailViewModel.cs
@@ -16,12 +16,14 @@ namespace JTO_WPF.ViewModels
         public UnitOfWork unit = new UnitOfWork(new JTOContext());
         public Theme Theme { get; set; }
         public string Name { get; set; }
+        public string Mode { get; set; }
         public DashboardViewModel DVM { get; set; }
 
         public ThemeDetailViewModel(DashboardViewModel dvm)
         {
             DVM = dvm;
             Theme = new Theme();
+            Mode = "Toevoegen";
             Name = "";
         }
 
@@ -29,6 +31,7 @@ namespace JTO_WPF.ViewModels
         {
             DVM = dvm;
             Theme = theme;
+            Mode = "Wijzig";
             Name = Theme.Name;
         }
 
@@ -50,15 +53,27 @@ namespace JTO_WPF.ViewModels
             switch (parameter.ToString())
             {
                 case "Add":
-                    Theme t = new Theme(Name);
-                    unit.ThemeRepo.Create(t);
-                    unit.Save();
-                    break;
-                case "Cancel":
+                    if(Theme.Name == null)
+                    {
+                        Theme t = new Theme(Name);
+                        unit.ThemeRepo.Create(t);
+                        unit.Save();
+                    } else
+                    {
+                        Theme.Name = Name;
+                        unit.ThemeRepo.Update(Theme);
+                        unit.Save();
+                    }
                     var tvm = new ThemeViewModel(DVM);
                     var tv = new ThemeView();
                     tv.DataContext = tvm;
                     DVM.Content = tv;
+                    break;
+                case "Cancel":
+                    var tvm2 = new ThemeViewModel(DVM);
+                    var tv2 = new ThemeView();
+                    tv2.DataContext = tvm2;
+                    DVM.Content = tv2;
                     break;
             }
         }
