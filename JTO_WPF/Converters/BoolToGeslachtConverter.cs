d7a163429af9184254095c5ef11deb5a29ea6f21
Kevin <r0389573@student.thomasmore.be>
2023-06-07 20:48:53 +0200

    added geslacht converter

---
+++ JTO_WPF/Converters/BoolToGeslachtConverter.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Data.UnitOfWork;
+using DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace JTO_WPF.Converters
+{
+    internal class BoolToGeslachtConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool geslacht)
+                if (geslacht)
+                {
+                    return "Man";
+                }
+                else
+                {
+                    return "Vrouw";
+                }
+
+            return "Onbekend";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
