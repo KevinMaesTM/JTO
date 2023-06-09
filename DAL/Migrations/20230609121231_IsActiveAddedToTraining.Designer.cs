1239c7caed6c672024090679901d9f9b904af513
R0294410 <jeroen-baeten@hotmail.com>
2023-06-09 14:13:01 +0200

    Added IsActive attribute to trainings

--- DAL/Migrations/20230609121231_IsActiveAddedToTraining.Designer.cs
+++ DAL/Migrations/20230609121231_IsActiveAddedToTraining.Designer.cs
@@ -4,6 +4,7 @@ using DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JTO_DAL.Migrations
 {
     [DbContext(typeof(JTOContext))]
-    partial class JTOContextModelSnapshot : ModelSnapshot
+    [Migration("20230609121231_IsActiveAddedToTraining")]
+    partial class IsActiveAddedToTraining
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
