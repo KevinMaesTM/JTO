b87d6ab25b62ec2fc5f91ec0c672c01c76ef3030
R0294410 <jeroen-baeten@hotmail.com>
2023-06-11 19:29:30 +0200

    Added Create and Update operations to Person object

--- JTO_WPF/ViewModels/PersonDetailViewModel.cs
+++ JTO_WPF/ViewModels/PersonDetailViewModel.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using JTO_MODELS;
 using System.Xml.Linq;
 using JTO_WPF.Views;
+using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace JTO_WPF.ViewModels
 {
@@ -43,41 +45,45 @@ namespace JTO_WPF.ViewModels
 
         public override bool CanExecute(object parameter)
         {
-            switch (parameter.ToString())
-            {
-                case "Add":
-                    return true;
-
-                case "Cancel":
-                    return true;
-
-                default:
-                    return false;
-            }
+            return true;
         }
 
         public override void Execute(object parameter)
         {
+            string errors = "";
             switch (parameter.ToString())
             {
-                case "Add":
-                    if (Person.Name == null)
+                case "Save":
+                    errors = ValidateInput();
+                    if (string.IsNullOrEmpty(errors))
                     {
-                        Person t = new Person();
-                        unit.PersonRepo.Create(t);
-                        unit.Save();
+                        if (SelectedSex == "Man")
+                            Person.Sex = true;
+                        else if (SelectedSex == "Vrouw")
+                            Person.Sex = false;
+
+                        if (Person.PersonID == 0)
+                        {
+                            unit.PersonRepo.Create(Person);
+                            unit.Save();
+                        }
+                        else
+                        {
+                            Person.Name = Name;
+                            unit.PersonRepo.Update(Person);
+                            unit.Save();
+                        }
+                        var pvm = new PersonViewModel(DVM);
+                        var pv = new PersonView();
+                        pv.DataContext = pvm;
+                        DVM.Content = pv;
+                        break;
                     }
                     else
                     {
-                        Person.Name = Name;
-                        unit.PersonRepo.Update(Person);
-                        unit.Save();
+                        MessageBox.Show(errors, "Oh no, errors!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
                     }
-                    var pvm = new PersonViewModel(DVM);
-                    var pv = new PersonView();
-                    pv.DataContext = pvm;
-                    DVM.Content = pv;
-                    break;
 
                 case "Cancel":
                     var pvm2 = new PersonViewModel(DVM);
@@ -86,6 +92,47 @@ namespace JTO_WPF.ViewModels
                     DVM.Content = pv2;
                     break;
             }
+        }
+
+        public string ValidateInput()
+        {
+            string result = "";
+            if (string.IsNullOrEmpty(Person.Name))
+                result += "Voornaam is een verplicht veld!" + Environment.NewLine;
+            if (string.IsNullOrEmpty(Person.Surname))
+                result += "Famillienaam is een verplicht veld!" + Environment.NewLine;
+            if (SelectedSex == null)
+                result += "Gelieve een geslacht te selecteren." + Environment.NewLine;
+            if (string.IsNullOrEmpty(Person.Street))
+                result += "Straat is een verplicht veld!" + Environment.NewLine;
+
+            if (string.IsNullOrEmpty(Person.Number))
+                result += "Huisnummer is een verplicht veld!" + Environment.NewLine;
+            else if (!Person.Number.Any(char.IsDigit))
+                result += "Huisnummer moet een nummer (0-9) bevatten!" + Environment.NewLine;
+
+            if (string.IsNullOrEmpty(Person.Zip))
+                result += "Postcode is een verplicht veld!" + Environment.NewLine;
+            else if (!Person.Zip.Any(char.IsDigit))
+                result += "Postcode moet een nummer (0-9) bevatten!" + Environment.NewLine;
+
+            if (string.IsNullOrEmpty(Person.City))
+                result += "Gemeente is een verplicht veld!" + Environment.NewLine;
+            if (string.IsNullOrEmpty(Person.Country))
+                result += "Land is een verplicht veld!" + Environment.NewLine;
+            if (string.IsNullOrEmpty(Person.Email) && string.IsNullOrEmpty(Person.Phone))
+                result += "Gelieve een email of telefoonnummer in te vullen." + Environment.NewLine;
+            else
+            {
+                if (!string.IsNullOrEmpty(Person.Email) && !Regex.IsMatch(Person.Email, "^\\S+@\\S+\\.\\S+$"))
+                    result += "Het ingevulde emailadres is ongeldig!." + Environment.NewLine;
+                if (!string.IsNullOrEmpty(Person.Phone) && Person.Phone.Count(n => Char.IsLetter(n)) >= 1)
+                    result += "Ingevuld telefoonnummer heeft een ongeldig formaat: Mag geen letters bevatten!" + Environment.NewLine;
+                if (!string.IsNullOrEmpty(Person.Phone) && (Person.Phone.Count(n => Char.IsNumber(n)) < Person.Phone.Length - 1))
+                    result += "Ingevuld telefoonnummer heeft een ongeldig formaat: Mag maximum 1 niet-numeriek character hebben!" + Environment.NewLine;
+            }
+
+            return result;
         }
     }
 }
