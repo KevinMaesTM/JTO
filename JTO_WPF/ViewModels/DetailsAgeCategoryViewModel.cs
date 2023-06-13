8bd579908fe21b6c17a50cfdf9538195fba67d61
R0294410 <jeroen-baeten@hotmail.com>
2023-06-13 11:33:46 +0200

    Refactorred AgeCategoryDetails ViewModel

--- JTO_WPF/ViewModels/DetailsAgeCategoryViewModel.cs
+++ JTO_WPF/ViewModels/DetailsAgeCategoryViewModel.cs
@@ -21,13 +21,11 @@ namespace JTO_WPF.ViewModels
         {
             AgeCategory = new AgeCategory();
             DVM = dVM;
-            AgeCategory.MinAge = null;
-            AgeCategory.MaxAge = null;
         }
 
         public DetailsAgeCategoryViewModel(DashboardViewModel dVM, AgeCategory ageCategory)
         {
-            this.AgeCategory = ageCategory;
+            AgeCategory = ageCategory;
             DVM = dVM;
         }
 
@@ -36,14 +34,20 @@ namespace JTO_WPF.ViewModels
             switch (parameter.ToString())
             {
                 case "Save":
-                    return true;
-
                 case "Cancel":
                     return true;
 
                 default:
                     return false;
             }
+        }
+
+        public void CreateAgeCategory()
+        {
+            unit.AgeCategoryRepo.Create(AgeCategory);
+            unit.Save();
+
+            DVM.SnackbarContent = $"Niewe leeftijdscategorie '{AgeCategory.MinAge}j - {AgeCategory.MaxAge}j' aangemaakt.";
         }
 
         public override void Execute(object parameter)
@@ -58,50 +62,47 @@ namespace JTO_WPF.ViewModels
                     {
                         // Check if it's a new AgeCategory
                         if (AgeCategory.AgeCategoryID == 0)
-                        {
-                            unit.AgeCategoryRepo.Create(AgeCategory);
-                            unit.Save();
-
-                            DVM.SnackbarContent = $"Niewe leeftijdscategorie '{AgeCategory.MinAge}j - {AgeCategory.MaxAge}j' aangemaakt.";
-                        }
+                            CreateAgeCategory();
                         else
-                        {
-                            unit.AgeCategoryRepo.Update(AgeCategory);
-                            unit.Save();
+                            UpdateAgeCategory();
 
-                            DVM.SnackbarContent = $"Leeftijdscategorie '{AgeCategory.MinAge}j - {AgeCategory.MaxAge}j' aangepast.";
-                        }
-                        var acVM = new AgeCategoryViewModel(DVM);
-                        var acV = new AgeCategoryView();
-                        acV.DataContext = acVM;
-                        DVM.Content = acV;
-                        break;
+                        ShowAgeCategories();
                     }
                     else
-                    {
                         MessageBox.Show(errors, "Errors!", MessageBoxButton.OK, MessageBoxImage.Error);
-                        break;
-                    }
-
-                case "Cancel":
-                    var avm2 = new AgeCategoryViewModel(DVM);
-                    var av2 = new AgeCategoryView();
-                    av2.DataContext = avm2;
-                    DVM.Content = av2;
                     break;
+
+                case "Cancel": ShowAgeCategories(); break;
+                default: break;
             }
+        }
+
+        public void ShowAgeCategories()
+        {
+            var acVM = new AgeCategoryViewModel(DVM);
+            var acV = new AgeCategoryView();
+            acV.DataContext = acVM;
+            DVM.Content = acV;
+        }
+
+        public void UpdateAgeCategory()
+        {
+            unit.AgeCategoryRepo.Update(AgeCategory);
+            unit.Save();
+
+            DVM.SnackbarContent = $"Leeftijdscategorie '{AgeCategory.MinAge}j - {AgeCategory.MaxAge}j' aangepast.";
         }
 
         public string ValidateInput()
         {
             string result = "";
             // Check input value of MinAge
-            if (AgeCategory.MinAge == null)
+            if (string.IsNullOrEmpty(AgeCategory.MinAge.ToString()))
                 result += "Minimum leeftijd is een verplicht veld." + Environment.NewLine;
             else if (!int.TryParse(AgeCategory.MinAge.ToString(), out int minAge))
                 result += "Minimum leeftijd is geen geldig getal!" + Environment.NewLine;
             // Check input value of MaxAge
-            if (AgeCategory.MaxAge == null)
+            if (string.IsNullOrEmpty(AgeCategory.MaxAge.ToString()))
                 result += "Maximum leeftijd is een verplicht veld." + Environment.NewLine;
             else if (!int.TryParse(AgeCategory.MaxAge.ToString(), out int maxAge))
                 result += "Maximum leeftijd is geen geldig getal!" + Environment.NewLine;
